2e40b52233b2660a95f07073f7b1a35eaf3d295f
SEP4000 <114220091+SEP4000@users.noreply.github.com>
2024-02-23 09:28:23 +0100

    Update ListCommand.java

--- src/main/java/com/fges/todoapp/ListCommand.java
+++ src/main/java/com/fges/todoapp/ListCommand.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.MissingNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -14,35 +15,28 @@ import java.util.stream.Collectors;
 public class ListCommand implements Command {
     @Override
     public void execute(String fileName, String fileContent, List<String> positionalArgs) throws IOException {
+        boolean showDone = positionalArgs.contains("-d"); // Check if --done flag is provided
+
         if (fileName.endsWith(App.JSON_EXTENSION)) {
-            handleJsonList(fileContent);
+            handleJsonList(fileContent, showDone);
         } else if (fileName.endsWith(App.CSV_EXTENSION)) {
             handleCsvList(fileContent);
         }
     }
 
-    private void handleJsonList(String fileContent) throws IOException
-    {
-        // Implémentation de la gestion des fichiers JSON
+    private void handleJsonList(String fileContent, boolean showDone) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode actualObj = mapper.readTree(fileContent);
-        if (actualObj instanceof MissingNode)
-        {
-            actualObj = JsonNodeFactory.instance.arrayNode();
-        }
+        ArrayNode todosArray = (ArrayNode) mapper.readTree(fileContent);
 
-        if (actualObj instanceof ArrayNode arrayNode)
-        {
-            arrayNode.forEach(node -> System.out.println("- " + node.toString()));
-        }
+        todosArray.forEach(todo -> {
+            boolean done = todo.get("done").asBoolean();
+            if (!showDone || done) { // Check if todo should be displayed based on showDone flag
+                System.out.println("- " + (done ? " (Done)" : ""));
+            }
+        });
     }
 
-    private void handleCsvList(String fileContent)
-    {
-        // Implémentation de la gestion des fichiers CSV
-        System.out.println(Arrays.stream(fileContent.split("\n"))
-                .map(todo -> "- " + todo)
-                .collect(Collectors.joining("\n"))
-        );
+    private void handleCsvList(String fileContent) {
+        System.out.println(fileContent.trim());
     }
 }
